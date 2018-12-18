8d1a546423d7c986a22e407f8423dd2e4e66c6d7
amir <afwan70@gmail.com>
2018-12-18 21:44:43 +0800

    design Meal List Layout

---
+++ FoodTaskerIOS/CustomerMenuTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CustomerMenuTableViewController.swift
+//  FoodTaskerIOS
+//
+//  Created by AMIR AFWAN on 12/18/18.
+//  Copyright © 2018 AMIR AFWAN. All rights reserved.
+//
+
+import UIKit
+
+class CustomerMenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 0.19, green: 0.18, blue: 0.31, alpha: 1.0)
+
+    }
+
+
+}
