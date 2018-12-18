8dd3ef12e815fb72e140afc3215aa4db4051e9cb
amir <afwan70@gmail.com>
2018-12-18 14:45:39 +0800

    create class for restaurant view controller and customer view

--- FoodTaskerIOS/RestaurantViewController.swift
+++ FoodTaskerIOS/RestaurantViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 
 class RestaurantViewController: UIViewController {
 
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        menuBarButton.target = self.revealViewController()
+        menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
+
+extension RestaurantViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        
+        return cell
+    }
+    
+}
+
+
