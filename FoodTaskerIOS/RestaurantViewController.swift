23a8c96172fb57da1dc5e90354f81a8381f0ca7c
amir <afwan70@gmail.com>
2018-12-18 12:06:22 +0800

    login, restaurant list UI, side menu

---
+++ FoodTaskerIOS/RestaurantViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantViewController.swift
+//  FoodTaskerIOS
+//
+//  Created by AMIR AFWAN on 12/18/18.
+//  Copyright © 2018 AMIR AFWAN. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
