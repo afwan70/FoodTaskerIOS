6eb0c63dd7af3857aaeccf85a1bdd988e386f8bd
amir <afwan70@gmail.com>
2018-12-18 22:42:57 +0800

    design meal detail layout

---
+++ FoodTaskerIOS/MealDetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MealDetailsViewController.swift
+//  FoodTaskerIOS
+//
+//  Created by AMIR AFWAN on 12/18/18.
+//  Copyright © 2018 AMIR AFWAN. All rights reserved.
+//
+
+import UIKit
+
+class MealDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+
+}
