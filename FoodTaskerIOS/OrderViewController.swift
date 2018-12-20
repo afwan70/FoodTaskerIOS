7cbaceefc3e188dcce990b8879fede60ea8dc06f
amir <afwan70@gmail.com>
2018-12-20 09:15:23 +0800

    Install Alamofire, SwiftJSON and Facebook SDK

---
+++ FoodTaskerIOS/OrderViewController.swift
@@ -0,0 +1,44 @@
+//
+//  OrderViewController.swift
+//  FoodTaskerIOS
+//
+//  Created by AMIR AFWAN on 12/20/18.
+//  Copyright © 2018 AMIR AFWAN. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuBarButton.target = self.revealViewController()
+            menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+    }
+    
+
+}
+
+extension OrderViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderItemCell", for: indexPath)
+        
+        return cell
+    }
+}
